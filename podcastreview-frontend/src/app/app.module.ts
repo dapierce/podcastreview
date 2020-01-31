ac5c31e86875551bbd7351d1f6a61a9c7a57ed07
dapierce <davidaaronpierce@gmail.com>
2020-01-31 00:46:52 -0800

    Added HTML, styling to Podcast Details component. - Currently have Review List in it, but we may want to seperate that into its own component in future. - Additional CSS fixes to Header.

--- podcastreview-frontend/src/app/app.module.ts
+++ podcastreview-frontend/src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser"
+import { NgModule } from "@angular/core"
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { PodcastDetailsComponent } from './components/podcast-details/podcast-details.component';
-import { ListPodcastsComponent } from './components/list-podcasts/list-podcasts.component';
+import { AppRoutingModule } from "./app-routing.module"
+import { AppComponent } from "./app.component"
+import { HeaderComponent } from "./components/header/header.component"
+import { FooterComponent } from "./components/footer/footer.component"
+import { PodcastDetailsComponent } from "./components/podcast-details/podcast-details.component"
+import { ListPodcastsComponent } from "./components/list-podcasts/list-podcasts.component"
 
 @NgModule({
   declarations: [
@@ -14,13 +14,10 @@ import { ListPodcastsComponent } from './components/list-podcasts/list-podcasts.
     HeaderComponent,
     FooterComponent,
     PodcastDetailsComponent,
-    ListPodcastsComponent
+    ListPodcastsComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
