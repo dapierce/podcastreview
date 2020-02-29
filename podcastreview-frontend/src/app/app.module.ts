5be5094f68e8039d6bde3fcd5b68f96e671da2ad
dapierce <davidaaronpierce@gmail.com>
2020-02-28 23:48:05 -0800

    Frontend app component passes title to header.

--- podcastreview-frontend/src/app/app.module.ts
+++ podcastreview-frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser"
 import { NgModule } from "@angular/core"
+import { HttpClientModule } from "@angular/common/http"
 
 import { AppRoutingModule } from "./app-routing.module"
 import { AppComponent } from "./app.component"
@@ -16,7 +17,7 @@ import { ListPodcastsComponent } from "./components/list-podcasts/list-podcasts.
     PodcastDetailsComponent,
     ListPodcastsComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- podcastreview-frontend/src/app/components/header/header.component.ts
+++ podcastreview-frontend/src/app/components/header/header.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core"
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"],
 })
 export class HeaderComponent implements OnInit {
+  @Input() title: string
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
