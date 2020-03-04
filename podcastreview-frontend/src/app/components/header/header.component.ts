5bd7ea04b8aba65a2012b4bb69a89f8d8b9aa5e0
dapierce <davidaaronpierce@gmail.com>
2020-03-03 23:33:14 -0800

    Header search bar action + login.

--- podcastreview-frontend/src/app/components/header/header.component.ts
+++ podcastreview-frontend/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from "@angular/core"
+import { Router } from "@angular/router"
 
 @Component({
   selector: "app-header",
@@ -8,7 +9,16 @@ import { Component, Input, OnInit } from "@angular/core"
 export class HeaderComponent implements OnInit {
   @Input() title: string
 
-  constructor() {}
+  constructor(private router: Router) {}
+
+  searchAction(input: string): void {
+    const sanitized = input.replace(/\W_/g, " ")
+    this.router.navigate(["/search/" + sanitized])
+  }
+
+  loginAction(): void {
+    this.router.navigate(["/login"])
+  }
 
   ngOnInit() {}
 }
